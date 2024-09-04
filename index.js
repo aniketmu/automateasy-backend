f9fb9bab9b2a35caa979b04182ce05102b12a771
Aniket Mukherjee <aniketmukherjee5@gmail.com>
2024-09-04 12:30:54 +0530

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 import express from 'express';
 import { DB } from './connect.js';
-const app = express()
-const PORT = 4000
+import mongoose from 'mongoose';
 
-DB;
+const app = express();
+const PORT = 4000;
 
-app.get('/', (req, res)=>{
-    res.send("Hello from Server")
+const userSchema = new mongoose.Schema({
+    userId: { type: Number, unique: true },
+    firstName: String,
+    lastName: String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+    res.send("Hello from Server");
+});
+
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find().limit(500); 
+        res.json(users);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).send("Error fetching users");
+    }
+});
+
+
+app.get("/users/:id", async(req,res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findOne({userId: id});
+        if(!user){
+            return res.status(404).send('User not found')
+        }
+        res.json(user);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).send("Error fetching User");
+    }
 })
 
-app.listen(PORT, () => {
-    console.log("Server running on PORT: " + PORT)
-})
+DB.then(() => {
+    app.listen(PORT, () => {
+        console.log("Server running on PORT: " + PORT);
+    });
+}).catch((err) => {
+    console.error("Failed to connect to the database", err);
+    process.exit(1);
+});
